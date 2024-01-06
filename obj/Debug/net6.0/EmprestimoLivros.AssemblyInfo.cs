6eaab780cd9777fe6093bc559ef90d0d5ddca564
Carlos77-code <carloslimafernandes@hotmail.com>
2024-01-05 22:48:14 -0300

    inserido ícons no buttons

--- obj/Debug/net6.0/EmprestimoLivros.AssemblyInfo.cs
+++ obj/Debug/net6.0/EmprestimoLivros.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EmprestimoLivros")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c617534134b171515f804f6374dc5afc22a1f855")]
 [assembly: System.Reflection.AssemblyProductAttribute("EmprestimoLivros")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EmprestimoLivros")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
